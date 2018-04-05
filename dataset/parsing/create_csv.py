6ce19afe9dd18087a745ce2f4eb61969167945a4
Invernizzi Hakim <hakim.invernizzi@epfl.ch>
2018-04-05 17:06:23 +0200

    doexploratory data analysis concerning genres

--- dataset/parsing/create_csv.py
+++ dataset/parsing/create_csv.py
@@ -32,3 +32,32 @@ df.tgdb_releasedate = pd.to_datetime(df.tgdb_releasedate)
 df = df[['brand', 'console', 'game', 'title', 'file_name', 'tgdb_genres', 'tgdb_id', 'tgdb_developer', 'tgdb_publisher', 'tgdb_platform', 'tgdb_gametitle', 'tgdb_releasedate', 'tgdb_players', 'tgdb_rating', 'tgdb_esrb', 'tgdb_overview']]
 
 df.to_csv('midi_dataframe.csv', index=False)
+
+data = []
+
+for midi in midi_db:
+    midi_data = {'brand': midi['brand'],
+                 'console': midi['console'],
+                 'game': midi['game'],
+                 'title': midi['title'],
+                 'file_name': midi['path'].split('/')[-1]
+                }
+    key = (consoles_mapping[midi['console']], midi['game'])
+    if key in game_data:
+        for info in ['Developer', 'ESRB', 'GameTitle', 'Overview', 'Platform', 'Players', 'Publisher', 'Rating', 'ReleaseDate', 'id']:
+            midi_data['tgdb_%s'%(info.lower())] = game_data[key].get(info)
+        if 'Genres' in game_data[key]:
+            genres = game_data[key]['Genres'].get('genre')
+            if isinstance(genres, str):
+                midi_data['tgdb_genres'] = genres
+                data.append(midi_data)
+            else:
+                for genre in genres:
+                    midi_data = midi_data.copy()
+                    midi_data['tgdb_genres'] = genre
+                    data.append(midi_data)
+df = pd.DataFrame(data)
+df.tgdb_releasedate = pd.to_datetime(df.tgdb_releasedate)
+df = df[['brand', 'console', 'game', 'title', 'file_name', 'tgdb_genres', 'tgdb_id', 'tgdb_developer', 'tgdb_publisher', 'tgdb_platform', 'tgdb_gametitle', 'tgdb_releasedate', 'tgdb_players', 'tgdb_rating', 'tgdb_esrb', 'tgdb_overview']]
+
+df.to_csv('midi_dataframe_genres.csv', index=False)
